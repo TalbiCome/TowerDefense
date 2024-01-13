d1436501d5cd96efde5c7b6dc8c727cbc809f56b
Kazenoyama <kishan.turpin@gmail.com>
2024-01-13 15:11:15 +0100

    Make a 60 FPS animation for the grid

--- src/main/java/org/example/TestJPanel.java
+++ src/main/java/org/example/TestJPanel.java
@@ -1,12 +1,23 @@
 package org.example;
 
 import javax.swing.JPanel;
+import java.awt.Color;
 import java.awt.Graphics;
+import java.util.Random;
 
 
 public class TestJPanel extends JPanel {
 
+    private Random random;
+    private long lastTime;
+    private int frame;
+
+
+
     public TestJPanel(){
+        random = new Random();
+
+
 
     }
 
@@ -16,9 +27,31 @@ public class TestJPanel extends JPanel {
 
         for(int x =0; x < 20; x++){
             for(int y = 0; y < 20; y++){
-                g.drawRect(x*32,y*32,32,32);
+                g.setColor(randomColor());
+                g.fillRect(x*32,y*32,32,32);
             }
         }
+
+
+        FPSCounter();
+
+    }
+
+    private void FPSCounter(){
+        frame++;
+        if(System.currentTimeMillis() - lastTime >= 1000){
+            System.out.println("FPS : " + frame);
+            frame = 0;
+            lastTime = System.currentTimeMillis();
+        }
+    }
+
+    public Color randomColor(){
+        int r = random.nextInt(256);
+        int g = random.nextInt(256);
+        int b = random.nextInt(256);
+
+        return new Color(r,g,b);
 
     }
 
--- src/main/java/org/example/TestJframe.java
+++ src/main/java/org/example/TestJframe.java
@@ -1,24 +1,46 @@
 package org.example;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class TestJframe extends JFrame{
 
     private TestJPanel jpanel;
+    private double timePerFrame;
+    private long lastFrame;
 
-    public TestJframe(){
-        setSize(640,640);
-        setVisible(true);
+    public TestJframe() {
+
+        setSize(640, 640);
+
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
         jpanel = new TestJPanel();
         add(jpanel);
 
+        timePerFrame = 1000000000.0 / 60.0; // 1 000 000 000 can change FPS for the game
 
+        setVisible(true);
+    }
+
+    private void loopGame(){
+        while(true) {
+            if (System.nanoTime() - lastFrame >= timePerFrame) {
+                lastFrame = System.nanoTime();
+                repaint();
+            } else {
+                //Do nothing
+            }
+        }
     }
 
     public static void main(String[] args){
         TestJframe jframe = new TestJframe();
+        jframe.loopGame();
 
 
     }

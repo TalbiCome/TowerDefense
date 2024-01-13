f3b6c2c7393473dc90e2f22c7c2661083ed85ee0
Côme Talbi <117739015+TalbiCome@users.noreply.github.com>
2024-01-13 16:54:37 +0100

    Documentation for Pos class

--- src/main/java/org/example/Pos.java
+++ src/main/java/org/example/Pos.java
@@ -1,24 +1,44 @@
 package org.example;
 
 public class Pos {
+    /*** x-axis value of the position*/
     int x;
+    /*** y-axis value of the position*/
     int y;
+    /*** maxBound corresponding to the screen size in pixels*/
     static final int maxBoundX = 1920;
+    /*** maxBound corresponding to the screen size in pixels*/
     static final int maxBoundY = 1080;
+    /*** minBound corresponding to the screen size in pixels*/
     static final int minBoundX = 0;
+    /*** minBound corresponding to the screen size in pixels*/
     static final int minBoundY = 0;
 
-    public boolean isPosOutOfBoundX()
+    /**
+     * is x value out of bound
+     * @return true if yes, else return false
+     */
+    protected boolean isPosOutOfBoundX()
     {
         return x > maxBoundX || x  < 0;
     }
 
-    public boolean isPosOutOfBoundY()
+    /**
+     * is y value out of bound
+     * @return true if yes, else return false
+     */
+    protected boolean isPosOutOfBoundY()
     {
         return y > maxBoundY || y < 0;
     }
 
-    void move(int xMovement, int yMovement)
+    /**
+     * Move the actual position by adding the value in parameters to each axis. If the value exceed
+     * the max/min bound the set the value on the according bound
+     * @param xMovement will be added to the actual x-axis position
+     * @param yMovement will be added to the actual y-axis position
+     */
+    public void move(int xMovement, int yMovement)
     {
         x += xMovement;
         y += yMovement;

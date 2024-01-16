408139d89d5fbcea70b41211cb8ed1438a5f392d
Côme Talbi <117739015+TalbiCome@users.noreply.github.com>
2024-01-16 16:47:42 +0100

    Enemy comments

--- src/main/java/org/example/SlowEnemy.java
+++ src/main/java/org/example/SlowEnemy.java
@@ -1,5 +1,7 @@
 package org.example;
-
+/**
+ * a slow enemy is an enemy with a lot of HP and a small amount of speed
+ */
 public class SlowEnemy extends Enemy{
     public SlowEnemy(Pos[] posArray, Strategy strat){
         super(1, 4, posArray, strat);

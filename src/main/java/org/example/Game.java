f2511442d121af54f610fbc89a78becaf84aa92a
Côme Talbi <117739015+TalbiCome@users.noreply.github.com>
2024-01-16 09:50:54 +0100

    game class

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -1,17 +1,17 @@
 package org.example;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Game {
 
     private static Game instance = null;
     ArrayList<Enemy> activeEnemys = new ArrayList<Enemy>();
-    ArrayList<Enemy> enemysQueue = new ArrayList<Enemy>();
+    Stack<Enemy> enemysQueue = new Stack<Enemy>();
     ArrayList<Tower> activeTowers = new ArrayList<Tower>();
     ElementsFactory elementsFactory = ElementsFactory.getInstance();
     int headQuarterHP = 30;
 
-    Level level;
+    Level level = null;
 
     protected Game(){};
 
@@ -44,9 +44,71 @@ public class Game {
         headQuarterHP = 30;
     }
 
-    public void update()
+    public boolean areAllEnemyDead()
     {
+        return enemysQueue.isEmpty() && activeEnemys.isEmpty();
+    }
 
+    public boolean didPlayerLost()
+    {
+        return headQuarterHP < 1;
+    }
+
+    public void resetLevel()
+    {
+        activeEnemys.clear();
+        activeTowers.clear();
+        enemysQueue.clear();
+        headQuarterHP = 30;
+    }
+
+    public void spawnNextEnemy()
+    {
+        if(!enemysQueue.isEmpty())
+        {
+            activeEnemys.add(enemysQueue.pop());
+        }
+    }
+
+    public void spawnTower(Pos pos)
+    {
+        activeTowers.add(new Tower(pos));
+    }
+
+    public void removeTower(Pos pos)
+    {
+        activeTowers.removeIf(towerInList -> Pos.arePosesEquals(towerInList.position, pos));
+    }
+
+    public void updateActiveEnemys()
+    {
+        ArrayList<Enemy> toDelete = new ArrayList<>();
+        for (Enemy enemy: activeEnemys) {
+            enemy.updateStatus();
+            if(enemy.isAtEnd)
+            {
+                headQuarterHP -= 1;
+                toDelete.add(enemy);
+            }
+            else if (enemy.isDead)
+            {
+                toDelete.add(enemy);
+            }
+        }
+        activeEnemys.removeAll(toDelete); //Evite les problèmes d'acces concourent
+        toDelete.clear();
+    }
+
+    public void updateTower()
+    {
+        boolean isTargetAvailable = !activeEnemys.isEmpty();
+        for (Tower tower: activeTowers) {
+            if(isTargetAvailable && tower.target == null)
+            {
+                tower.assignNewTarget(activeEnemys.get(0));
+            }
+            tower.shootAtTarget();
+        }
     }
 
 
---
+++ src/test/java/org/example/TestGame.java
@@ -0,0 +1,58 @@
+package org.example;
+import org.junit.Ignore;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestGame {
+
+    Game prepareTest()
+    {
+        Game game = Game.getInstance();
+        game.resetLevel();
+        game.loadLevel(1);
+        return game;
+    }
+    @Test
+    void levelLoadingTest()
+    {
+        Game game = prepareTest();
+        assertEquals(30, game.headQuarterHP);
+        assertTrue(game.activeEnemys.isEmpty());
+        assertTrue(game.activeTowers.isEmpty());
+    }
+
+    @Test
+    void GameTest()
+    {
+        Game game = prepareTest();
+        game.spawnTower(new Pos(0, 0));
+
+        for(int i = 0 ; i < 100; i++)
+        {
+            game.spawnNextEnemy();
+            game.updateActiveEnemys();
+            game.updateTower();
+        }
+        game.removeTower(new Pos(0, 0));
+        assertFalse(game.didPlayerLost());
+        assertTrue(game.areAllEnemyDead());
+        assertTrue(game.activeTowers.isEmpty());
+    }
+
+    @Test
+    void lostGameTest()
+    {
+        Game game = prepareTest();
+        game.headQuarterHP = 4;
+
+        for(int i = 0 ; i < 110; i++)
+        {
+            game.spawnNextEnemy();
+            game.updateActiveEnemys();
+            game.updateTower();
+        }
+        assertTrue(game.didPlayerLost());
+        assertTrue(game.areAllEnemyDead());
+    }
+}

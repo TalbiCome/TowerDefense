b89279bdde8a6aaa927abf666ea438baf038c429
Côme Talbi <117739015+TalbiCome@users.noreply.github.com>
2024-01-16 14:57:21 +0100

    main

--- src/main/java/org/example/Level.java
+++ src/main/java/org/example/Level.java
@@ -13,18 +13,52 @@ public class Level {
         this.posArray = posArray;
     }
 
-    public static Level loadLevel1()
+    public static Level loadTestLevel1()
     {
         Integer[] encodedEnemyList = {1, 2, 3, 1, 2, 3, 1, 2, 3};
         Pos[] posArray = {new Pos(0,0), new Pos(100, 0)};
         return new Level(encodedEnemyList, posArray);
     }
 
-    public static Level loadLevel2()
+    public static Level loadTestLevel2()
     {
         Integer[] encodedEnemyList = {1, 2, 3, 1, 2, 3, 1, 2, 3};
         Pos[] posArray = {new Pos(0,0), new Pos(100, 100)};
         return new Level(encodedEnemyList, posArray);
     }
+
+    public static Level loadLevel1()
+    {
+
+        Integer[] encodedEnemyList = {
+                1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ,1 ,1 ,1 ,1 ,1 ,1
+                , 1, 1, 1, 1, 1, 1, 1, 1, 1 ,1 ,1 ,1 ,1 ,1 ,1
+                , 1, 1, 1, 1, 1, 1, 1, 1, 1 ,1 ,1 ,1 ,1 ,1 ,1
+                , 1, 1, 1, 1, 1, 1, 1, 1, 1 ,1 ,1 ,1 ,1 ,1 ,1
+                , 1, 1, 1, 1, 1, 1, 1, 1, 1 ,1 ,1 ,1 ,1 ,1 ,1
+                , 1, 1, 1, 1, 1, 1, 1, 1, 1 ,1 ,1 ,1 ,1 ,1 ,1
+                , 1, 1, 1, 1, 1, 1, 1, 1, 1 ,1 ,1 ,1 ,1 ,1 ,1
+                , 1, 1, 1, 1, 1, 1, 1, 1, 1 ,1 ,1 ,1 ,1 ,1 ,1
+                , 1, 1, 1, 1, 1, 1, 1, 1, 1 ,1 ,1 ,1 ,1 ,1 ,1
+                , 1, 1, 1, 1, 1, 1, 1, 1, 1 ,1 ,1 ,1 ,1 ,1 ,1};
+        Pos[] posArray = {new Pos(0,0), new Pos(100, 100)};
+        return new Level(encodedEnemyList, posArray);
+    }
+
+    public static Level loadLevel2()
+    {
+
+        Integer[] encodedEnemyList = {
+                2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+                2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+                2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+                2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+                2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+                2, 2, 2, 2, 2, 2, 2, 2, 2, 2,};
+        Pos[] posArray = {new Pos(0,0), new Pos(50, 0), new Pos(50, 50), new Pos(100, 50)};
+        return new Level(encodedEnemyList, posArray);
+    }
+
+
 
 }

94dcdbfe5da36acd4e1790adba5608a629649128
Côme Talbi <117739015+TalbiCome@users.noreply.github.com>
2024-01-17 09:45:10 +0100

    strategy comment

--- src/main/java/org/example/FlyingStrategy.java
+++ src/main/java/org/example/FlyingStrategy.java
@@ -2,7 +2,17 @@ package org.example;
 
 import java.util.Stack;
 
+/**
+ * Flying strategy allow an enemy to move directly to the end of the path in a straight line
+ */
 public class FlyingStrategy implements Strategy{
+    /**
+     * move the enemy to the next position place at index 0 of the trailStack in a straight line by an increments of Enemy.speed
+     * If the enemy as no more position to go then update its boolean value isAtEnd end isDead to True
+     * If the enemy is not at the last position of the position stack then move the enemy
+     * Flying strategy defile the whole list and only keep the last position.
+     * @param enemy the enemy we want to move
+     */
     @Override
     public void moveToNextPos(Enemy enemy)
     {

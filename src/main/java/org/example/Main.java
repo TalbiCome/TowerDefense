a58763a125d2e6a31183c20920263c8f65d5a7c2
Côme Talbi <117739015+TalbiCome@users.noreply.github.com>
2024-01-18 10:26:20 +0100

    main menu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,24 @@
 package org.example;
+import java.sql.Time;
+import java.util.Scanner;
 public class Main {
+
+    public static int mainMenuUserInput()
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter something: ");
+        int userInput = scanner.nextInt();
+        System.out.println("You entered: " + userInput);
+        return userInput;
+    }
+
     public static boolean mainGameLoop(GameController gameController,int level)
     {
         System.out.println("loading level " + level);
         gameController.initGameLevel(1);
         while (!gameController.game.didPlayerLost() || !gameController.game.areAllEnemyDead())
         {
+            //coreler l'update avec l'image et la frequence d'update
             //regarder clique souris
             gameController.updateGameStatus();
         }
@@ -20,16 +33,28 @@ public class Main {
     public static void main(String[] args)
     {
         GameController gameController = new GameController();
-        boolean gameResult;
-
+        boolean gameResultPlayerLost = false;
         gameController.startWindow();
-        gameResult = mainGameLoop(gameController, 1);
-
-        if(gameResult)
+        int userInput = 0;
+        while(userInput != 2)
         {
-            deathScreen();
-        }
+            System.out.println("Main menu : \n1) load level 1\n2) close game");
+            userInput = mainMenuUserInput();
 
+            switch (userInput)
+            {
+                case 1:
+                    gameResultPlayerLost = mainGameLoop(gameController, 1);
+                    break;
+                case 2:
+                    System.out.println("closing game");
+                    return;
+            }
+            if(gameResultPlayerLost)
+            {
+                deathScreen();
+            }
+        }
     }
 
 
